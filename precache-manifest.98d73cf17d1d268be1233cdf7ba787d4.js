d7582e4c4e3e200fd7619042c577cd872e67b552
root <root@localhost.localdomain>
2020-04-27 02:50:00 +0530

    Updates

---
+++ precache-manifest.98d73cf17d1d268be1233cdf7ba787d4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f637e557136bc5876edfd876ebf96bab",
+    "url": "/myshopf/index.html"
+  },
+  {
+    "revision": "02e54483b546ce9d6e90",
+    "url": "/myshopf/static/css/main.80f1e444.chunk.css"
+  },
+  {
+    "revision": "d72f76e04e5ebd1077cf",
+    "url": "/myshopf/static/js/2.073d570b.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/myshopf/static/js/2.073d570b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e54483b546ce9d6e90",
+    "url": "/myshopf/static/js/main.53d4741a.chunk.js"
+  },
+  {
+    "revision": "3733ce73d08004dd2f25",
+    "url": "/myshopf/static/js/runtime-main.37cb5504.js"
+  },
+  {
+    "revision": "91dd83e97da05e66d840e6016f6a58f0",
+    "url": "/myshopf/static/media/banner-img.91dd83e9.png"
+  },
+  {
+    "revision": "45b22b534704b84b3d9202f3adccd855",
+    "url": "/myshopf/static/media/c1.45b22b53.jpg"
+  },
+  {
+    "revision": "8c5af8aa0227021b42b025a1ad8e4929",
+    "url": "/myshopf/static/media/c2.8c5af8aa.jpg"
+  },
+  {
+    "revision": "39d53fb72cf700df82d21e6ca7288667",
+    "url": "/myshopf/static/media/c3.39d53fb7.jpg"
+  },
+  {
+    "revision": "1a9cca0be07f856d94cbd381362e8a5f",
+    "url": "/myshopf/static/media/c4.1a9cca0b.jpg"
+  },
+  {
+    "revision": "6a02f45778f4b19aef062f87faf88890",
+    "url": "/myshopf/static/media/c5.6a02f457.jpg"
+  },
+  {
+    "revision": "4f51357f91f7f5d8fa3f749c7e445d9e",
+    "url": "/myshopf/static/media/cart.4f51357f.jpg"
+  },
+  {
+    "revision": "9df590d68ec0b77970ff5466c3001c48",
+    "url": "/myshopf/static/media/login.9df590d6.jpg"
+  },
+  {
+    "revision": "3b9eae59fe2e68f58022b1929f3a1dbc",
+    "url": "/myshopf/static/media/review-1.3b9eae59.png"
+  },
+  {
+    "revision": "046aed0d7cb5666f51fefeb7eda82f80",
+    "url": "/myshopf/static/media/review-2.046aed0d.png"
+  },
+  {
+    "revision": "4950bd7dbfb7dddfb5de19837fd3d1be",
+    "url": "/myshopf/static/media/review-3.4950bd7d.png"
+  }
+]);
